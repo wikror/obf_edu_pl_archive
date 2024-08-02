95ab9fe561947fa9592c885029f143ca675eaf6a
Wiktor Rorot <wiktor.rorot@gmail.com>
2024-08-02 15:39:19 +0200

    uploading the archive

---
+++ obf.edu.pl/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>http://obf.edu.pl/en</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://obf.edu.pl/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://obf.edu.pl/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://obf.edu.pl/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://obf.edu.pl/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://obf.edu.pl/en/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
